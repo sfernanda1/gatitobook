b8b085bc3eba45ef95e7bfb5b8084b5ef3471f71
Fernanda <sfernanda1@outlook.com>
2022-08-23 15:15:34 -0300

    primeiro commit

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,28 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  usuario= '';
+  senha= '';
+
+  constructor(private authService: AutenticacaoService, private router:Router) { }
+
+  ngOnInit(): void {}
+
+  login(){
+    this.authService.autentica(this.usuario,this.senha).subscribe(() =>{
+     this.router.navigate(['animais'])
+    },(error) => {
+        alert('usuário ou senha inválido');
+        console.log(error);
+      }
+    )
+  }
+
+}
